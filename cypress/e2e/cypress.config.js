b130ab59e4ba748756aa6fa8be8a611bf67f7f92
Your Name <youremail@example.com>
2024-02-05 16:34:13 +0100

    simulation on local env done

--- cypress/e2e/cypress.config.js
+++ cypress/e2e/cypress.config.js
@@ -2,8 +2,13 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    specPattern: '**/*.cy.{js,jsx,ts,tsx}', // Match all files in the e2e directory
+    supportFile: false , // Include directory path for e2e support file
+    // Other e2e-specific configurations if needed
+  },
+  component: {
+    specPattern: '**/*', // Match all files in the component directory
+    supportFile: false, // Include directory path for component support file
+    // Other component-specific configurations if needed
   },
 });
